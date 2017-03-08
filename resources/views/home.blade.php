b892e0c94151496798b8f4131021300460d4ffe7
johnweber <harlyseraphim@live.de>
2017-03-08 16:29:43 +0100

    fixed laravel bug --for php artisan migrate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <input type="button" value="Upload" onclick="showPicker()" />
                 </div>
             </div>
         </div>
